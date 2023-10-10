84584530ec161d167e9ff1135025ef209f91c4d4
Maxime Choffat <maxime.choffat@gmail.com>
2023-10-10 18:53:51 +0000

    language switcher

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { faSun as faSunReg, faMoon as faMoonReg } from '@fortawesome/free-regular-svg-icons';
-import { Theme } from 'src/models/theme';
 import { ThemeService } from 'src/services/theme.service';
 
 @Component({
@@ -13,13 +13,17 @@ export class HeaderComponent {
 
   @ViewChild("themeIcon", {read: ElementRef, static: true}) themeIcon: ElementRef;
 
+  availableLanguages = [
+    {name: "Français", key: "fr", flag: "🇫🇷"},
+    {name: "English", key: "en", flag: "🇬🇧"}
+  ]
 
   faSunSolid = faSun;
   faSunLine = faSunReg;
   faMoonSolid = faMoon;
   faMoonLine = faMoonReg;
 
-  constructor(protected themeService: ThemeService) {}
+  constructor(protected themeService: ThemeService, protected i18nService: TranslateService) {}
 
   toggleTheme() {
     this.themeService.toggle();
@@ -29,5 +33,11 @@ export class HeaderComponent {
     return this.themeService.activeTheme;
   }
 
+  toggleLanguage(lang: string) {
+    this.i18nService.use(lang)
+  }
 
+  get currentLanguage(): string {
+    return this.i18nService.currentLang;
+  }
 }
